aeea2b8706f5271e36d8c5a2931dae6e82fe3d55
Bert Outtier <outtierbert@gmail.com>
2019-02-28 08:21:12 +0100

    Fix libmendeleev and mqtt2mendeleev packages

--- package/libmendeleev/libmendeleev.mk
+++ package/libmendeleev/libmendeleev.mk
@@ -4,13 +4,11 @@
 #
 ################################################################################
 
-LIBMENDELEEV_VERSION = r10
-# LIBMENDELEEV_SITE = https://github.com/area3001/libmendeleev.git
-# LIBMENDELEEV_SITE_METHOD = git
-LIBMENDELEEV_SITE = /home/bert/area3001/libmendeleev
-LIBMENDELEEV_SITE_METHOD = local
+LIBMENDELEEV_VERSION = 131f08aedc97aa56937c2efe82c731f1ebba9259
+LIBMENDELEEV_SITE = $(call github,area3001,libmendeleev,$(LIBMENDELEEV_VERSION))
 LIBMENDELEEV_LICENSE = LGPL-2.1+
 LIBMENDELEEV_LICENSE_FILES = COPYING.LESSER
 LIBMENDELEEV_INSTALL_STAGING = YES
+LIBMENDELEEV_AUTORECONF = YES
 
 $(eval $(autotools-package))
