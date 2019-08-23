5d2fcb26a52d36a8a85fbd9004c69a4850ef131b
Bert Outtier <outtierbert@gmail.com>
2019-08-23 14:06:21 +0200

    Add package inputevent

---
+++ package/inputevent/inputevent.mk
@@ -0,0 +1,35 @@
+################################################################################
+#
+# inputevent
+#
+################################################################################
+
+INPUTEVENT_VERSION = r16
+INPUTEVENT_SITE = $(call github,kurt-vd,inputevent,$(INPUTEVENT_VERSION))
+INPUTEVENT_LICENSE = GPLv3
+INPUTEVENT_LICENSE_FILES = LICENSE
+
+define INPUTEVENT_CONFIGURE_CMDS
+	echo "PREFIX=/usr" > $(@D)/config.mk
+	echo "CFLAGS=$(TARGET_CFLAGS)" >> $(@D)/config.mk
+	echo "CPPFLAGS=-D_GNU_SOURCE $(TARGET_CPPFLAGS)" >> $(@D)/config.mk
+	echo "CXXFLAGS=$(TARGET_CXXFLAGS)" >> $(@D)/config.mk
+	echo "LDFLAGS=$(TARGET_LDFLAGS)" >> $(@D)/config.mk
+	echo "LDLIBS+=$(TARGET_LDLIBS)" >> $(@D)/config.mk
+	echo "CC=$(TARGET_CC)" >> $(@D)/config.mk
+	echo "CXX=$(TARGET_CXX)" >> $(@D)/config.mk
+	echo "LD=$(TARGET_LD)" >> $(@D)/config.mk
+	echo "AS=$(TARGET_AS)" >> $(@D)/config.mk
+
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) clean
+endef
+
+define INPUTEVENT_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define INPUTEVENT_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) install DESTDIR="$(TARGET_DIR)"
+endef
+
+$(eval $(generic-package))
