b26452827418636ec43743bf7a45831bcf6d41b8
Bert Outtier <outtierbert@gmail.com>
2019-09-07 07:26:42 +0200

    Improve push to trigger update

--- board/mendeleev/rootfs_overlay/usr/bin/update.sh
+++ board/mendeleev/rootfs_overlay/usr/bin/update.sh
@@ -8,7 +8,8 @@ BUTTON_KEYCODE="LEFT"
 DEVICE=ttyAMA0
 HOST=localhost
 PORT=1883
-UPDATEFILE=/tmp/release.bin
+
+LATESTURL=https://api.github.com/repos/area3001/ArduinoMendeleev/releases/latest
 
 # get settings from file in boot partition
 if [ -e /boot/settings ]; then
@@ -16,20 +17,21 @@ if [ -e /boot/settings ]; then
   . /boot/settings
 fi
 
-# Set LED mode to brightness
-setupled() {
+blinkled() {
   cd /sys/class/leds/$1
-  echo brightness > trigger
+  echo timer > trigger
+  echo $2 > delay_on
+  echo $2 > delay_off
 }
 
 # Set LED brightness value
 setled() {
   cd /sys/class/leds/$1
+  echo brightness > trigger
   echo $2 > brightness
 }
 
 # turn update led off
-setupled ext_led
 setled ext_led 0
 
 # Main program flow
@@ -37,16 +39,42 @@ while read TIME CODE VALUE; do
 case "${CODE}" in
   KEY_${BUTTON_KEYCODE}) # button
     if [ $VALUE == 1 ]; then # is pressed
-      echo "button pressed"
-      setled ext_led 1
+      blinkled ext_led 500
+      echo -en "\n\n" >> /updates/log
     else # is released
-      echo "button released"
+      echo "starting update..." >> /updates/log
+      if curl -L --show-error --fail -o /updates/latest.json ${LATESTURL}; then
+        # success
+        TAG=`jq -r .tag_name /updates/latest.json`
+        echo "starting update to version ${TAG}..." >> /updates/log
 
-      curl -o ${UPDATEFILE} https://github.com/area3001/ArduinoMendeleev/releases/download/latest/release.bin
-      # curl -o ${UPDATEFILE}.sha1 https://github.com/area3001/ArduinoMendeleev/releases/download/latest/release.bin.sha1
-      cat ${UPDATEFILE} | mosquitto_pub -h ${HOST}:${PORT} -t mendeleev/255/ota -s
+        if [ ! -d /updates/${TAG} ]; then
+          mkdir -p /updates/${TAG}
+          cd /updates/${TAG}
+          for ASSETURL in $(jq -r '.assets[].browser_download_url' /updates/latest.json); do
+            if curl -sLO --show-error --fail ${ASSETURL}; then
+              echo "downloaded ${ASSETURL}..." >> /updates/log
+            else
+              echo "Failed to download ${ASSETURL}!" >> /updates/log
+              blinkled ext_led 100
+              continue
+            fi
+          done
+        fi
 
-      setled ext_led 0
+        cd /updates/${TAG}
+        if sha1sum -sc release.bin.sha1; then
+          echo "Updating to ${TAG}..." >> /updates/log
+          cat /updates/${TAG}/release.bin | mosquitto_pub -h ${HOST}:${PORT} -t mendeleev/255/ota -s
+          setled ext_led 0
+        else
+          echo "Sha1 sum is not correct!" >> /updates/log
+          blinkled ext_led 100
+        fi
+      else
+        echo "Failed to get json!" >> /updates/log
+        blinkled ext_led 100
+      fi
     fi;
     ;;
   SYN_REPORT) # Ignore SYN_REPORT keycodes
