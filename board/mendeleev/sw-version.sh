c0aa78d4fbd260b0cfe2d13e168f2bfb7d51ba3a
Bert Outtier <outtierbert@gmail.com>
2020-09-17 14:25:59 +0200

    add sw version

---
+++ board/mendeleev/sw-version.sh
@@ -0,0 +1,7 @@
+# Add sw version to /etc/os-release
+VERSION=$(git --git-dir=../.git --work-tree=../ describe --tags --always --dirty --match "[0-9]*.[0-9]*.[0-9]*")
+DATE=$(d="$(git --git-dir=../.git log --date=iso --pretty=%ad -1)"; date --iso-8601=seconds --date="$d")
+
+echo "Building version: ${VERSION} of date ${DATE}"
+echo "MENDELEEV_VERSION=${VERSION}" >> "$TARGET_DIR/etc/os-release" 2> /dev/null
+echo "MENDELEEV_DATE=${DATE}" >> "$TARGET_DIR/etc/os-release" 2> /dev/null
