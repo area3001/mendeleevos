785e252db9998a5e3600787a2f487e527bfdbba0
Bert Outtier <outtierbert@gmail.com>
2023-05-17 15:32:26 +0200

    add rpi4 and cm4 support

---
+++ package/python-mendeleev/S90mqtt2mendeleev.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Starts mqtt2mendeleev on ${DEVICE}
+#
+
+DEVICE=ttyAMA0
+HOST=localhost
+LOGLEVEL=WARNING
+
+# Add /boot to fstab
+if [ -e /boot/settings ]; then
+	echo "Overriding defaults from /boot/settings"
+	. /boot/settings
+fi
+
+case "$1" in
+	start)
+		printf "Starting mqtt2mendeleev on ${DEVICE}..."
+		start-stop-daemon -q -S -b -m -p /var/run/mqtt2mendeleev_${DEVICE}.pid \
+			--exec /usr/bin/python3 -- /usr/bin/mqtt2mendeleev.py -b ${HOST} \
+			-d /dev/${DEVICE} -l ${LOGLEVEL} -f /var/log/mqtt2mendeleev
+		[ $? = 0 ] && echo "OK" || echo "FAIL"
+	;;
+	stop)
+		printf "Stopping mqtt2mendeleev on ${DEVICE}..."
+		start-stop-daemon -q -K -s SIGINT -p /var/run/mqtt2mendeleev_${DEVICE}.pid
+		[ $? = 0 ] && echo "OK" || echo "FAIL"
+	;;
+	restart|reload)
+		$0 stop
+		sleep 1
+		$0 start
+	;;
+	*)
+		echo "Usage: $0 {start|stop|restart}"
+		exit 1
+esac
+
+exit $?
